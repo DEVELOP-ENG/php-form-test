4963b5fe0832b39699faf1e1ae984c2b9c70a4f4
Noel Eng <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-06-18 10:35:29 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,7 +1,13 @@
-<?php 
-if(){
-
-}else{
-
+<?php
+if(isset($_GET["FirstName"])){//show data
+  echo $_GET["FirstName"];
+}else{//show form
+  echo
+  '
+  <form action="">
+  <P>First Name <input type="text" name="FirstName" /></p>
+  <p><input type="submit" /></p>
+  </form>
+  ';
 }
 ?>
